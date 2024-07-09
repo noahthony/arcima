4d023a34e644d2a2940950909cfcfddf76c259cc
Noah <103928293+noahthony@users.noreply.github.com>
2024-07-09 19:24:21 +0300

    Contact page update
    adding animations and dynamics to the page

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,58 @@
+
+
+// making each form appear according to the category selected
+const navigationRow = document.querySelector(".card-row");
+const navigationCards = document.querySelectorAll(".card")
+const navigation = [
+    {
+        menu: document.querySelector(".contact-navigation .archviz"),
+        form: document.querySelector(".archviz-form")
+    },
+    {
+        menu: document.querySelector(".contact-navigation .web-app"),
+        form: document.querySelector(".web-app-form")
+    },
+    {
+        menu: document.querySelector(".contact-navigation .cinematic"),
+        form: document.querySelector(".cinematic-form")
+    },
+    {
+        menu: document.querySelector(".contact-navigation .product-showcase"),
+        form: document.querySelector(".product-showcase-form")
+    }
+];
+
+
+
+for(let i = 0, c = navigation.length; i < c; i++){
+    navigation[i].menu.addEventListener('click', (e)=>{
+        
+        // keeping the menu being active bright and the others dim.
+        if(navigation[i].menu.classList.contains('active') && navigation[i].menu !== e.target){
+            navigation[i].menu.classList.add('inactive');
+        }
+        
+        
+        // display the corresponding form to the menu being clicked on 
+        if(navigation[i].form.classList.contains('hide')){
+            navigation[i].form.classList.toggle('hide');
+            navigation[i].menu.classList.toggle('active')
+        }
+        for(let i = 0; i <  c; i++){
+            if(navigation[i].menu !== e.target){
+                navigation[i].menu.classList.remove('active');
+                navigation[i].form.classList.add('hide');
+                navigation[i].menu.classList.add('inactive');
+            }
+        }
+    });
+}
+
+// 
+
+
+
+
 // getting the elements of the form that needs changes
 
 const inputQuestion = document.querySelectorAll('.project-info span[help]');
--- js/projects.js
+++ js/projects.js
@@ -5,23 +5,23 @@
 // getting all the necessary menu items
 
 
-const projectNavigationItems = document.querySelectorAll('.project-navigation > div');
-const projectNavigation = document.querySelector('.project-navigation');
+// const projectNavigationItems = document.querySelectorAll('.project-navigation > div');
+// const projectNavigation = document.querySelector('.project-navigation');
 
-for( let i = 0, c = projectNavigationItems.length, gridAtStart = 2, gridAtEnd = 6; i < c; i++){
-    projectNavigationItems[i].addEventListener('mousemove', (e)=> {
-        switch(i){
-            case i=0:
-                // projectNavigationItems[i+1].style = 'grid-column:'+ (gridAtEnd + i) + '/ span 4;'
-                projectNavigation.style = 'grid-template-columns: 400px repeat(11, 100px)';
-        }
+// for( let i = 0, c = projectNavigationItems.length, gridAtStart = 2, gridAtEnd = 6; i < c; i++){
+//     projectNavigationItems[i].addEventListener('mousemove', (e)=> {
+//         switch(i){
+//             case i=0:
+//                 // projectNavigationItems[i+1].style = 'grid-column:'+ (gridAtEnd + i) + '/ span 4;'
+//                 projectNavigation.style = 'grid-template-columns: 400px repeat(11, 100px)';
+//                 break;
+//             case i=1:
+//                 projectNavigation.style = 'grid-template-columns: 100px 400px repeat(10, 100px)';
+
+//         }
         
-    });
-    projectNavigationItems[i].addEventListener('mouseout', (e)=> {
-        switch(i){
-            case i=0:
-                projectNavigation.style= 'grid-template-columns: repeat(12, 100px);'
-        }
-        
-    })
-}
+//     });
+//     projectNavigationItems[i].addEventListener('mouseout', (e)=> {
+//         projectNavigation.style= 'grid-template-columns: repeat(12, 100px);';
+//     })
+// }
